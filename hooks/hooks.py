fce469ff70b2bce03551f2eca1d2ffe83f5d7389
Kapil Thangavelu <kapilt@gmail.com>
2014-10-26 17:54:44 -0400

    better minion registration and coordination with network overlay

--- hooks/hooks.py
+++ hooks/hooks.py
@@ -3,6 +3,8 @@
 import json
 import httplib
 import os
+import time
+import subprocess
 import sys
 import urlparse
 
@@ -11,62 +13,53 @@ from charmhelpers.core import hookenv, host
 hooks = hookenv.Hooks()
 
 
-@hooks.hook('config-changed')
-def config_changed():
-    """
-    Called whenever our service configuration changes.
-    """
-
-
-@hooks.hook('start')
-def start():
-    """
-    Start called once on initial installation to start services,
-    and subsequently once per boot.
-    """
-
-
-@hooks.hook('etcd-relation-changed', 'api-relation-changed')
+@hooks.hook('etcd-relation-changed',
+            'api-relation-changed',
+            'network-relation-changed')
 def relation_changed():
     """Connect the parts and go :-)
     """
     template_data = get_template_data()
 
     # Check required keys
-    for k in ('etcd_servers', 'kubeapi_server'):
+    for k in ('etcd_servers', 'kubeapi_server', 'overlay_type'):
         if not template_data.get(k):
-            print "missing required data", k, template_data
+            print("Missing data for %s %s" % (k, template_data))
             return
+    print("Running with\n%s" % template_data)
 
-    print "Running with\n", template_data
+    # Setup kubernetes supplemental group
+    setup_kubernetes_group()
 
-    # Register machine via api, not properly showing up.. afaics
-    #print "Registering machine"
-    #register_machine(template_data['kubeapi_server'])
+    # Register machine via api
+    print("Registering machine")
+    register_machine(template_data['kubeapi_server'])
 
     # Register services
     for n in ("cadvisor", "kubelet", "proxy"):
         if render_upstart(n, template_data) or not host.service_running(n):
-            print "Starting", n
+            print("Starting %s" % n)
             host.service_restart(n)
 
-    # Save the marker
+    # Save the marker (for restarts to detect prev install)
     template_data.save()
 
 
 def get_template_data():
     rels = hookenv.relations()
-
     template_data = hookenv.Config()
     template_data.CONFIG_FILE_NAME = ".unit-state"
 
+    overlay_type = get_scoped_rel_attr('network', rels, 'overlay_type')
     etcd_servers = get_rel_hosts('etcd', rels, ('hostname', 'port'))
-    api_servers = get_rel_hosts('api', rels)
+    api_servers = get_rel_hosts('api', rels, ('hostname', 'port'))
 
     # kubernetes master isn't ha yet.
     if api_servers:
-        api_servers = "http://%s:8080" % api_servers[0]
+        api_info = api_servers.pop()
+        api_servers = "http://%s:%s" % (api_info[0], api_info[1])
 
+    template_data['overlay_type'] = overlay_type
     template_data['kubelet_bind_addr'] = hookenv.unit_private_ip()
     template_data['proxy_bind_addr'] = hookenv.unit_get('public-address')
     template_data['kubeapi_server'] = api_servers
@@ -74,13 +67,24 @@ def get_template_data():
         'http://%s:%s' % (s[0], s[1]) for s in sorted(etcd_servers)])
     template_data['identifier'] = os.environ['JUJU_UNIT_NAME'].replace(
         '/', '-')
-    return template_data
+    return _encode(template_data)
 
 
 def _encode(d):
     for k, v in d.items():
         if isinstance(v, unicode):
             d[k] = v.encode('utf8')
+    return d
+
+
+def get_scoped_rel_attr(rel_name, rels, attr):
+    private_ip = hookenv.unit_private_ip()
+    for r, data in rels.get(rel_name, {}).items():
+        for unit_id, unit_data in data.items():
+            if unit_data.get('private-address') != private_ip:
+                continue
+            if unit_data.get(attr):
+                return unit_data.get(attr)
 
 
 def get_rel_hosts(rel_name, rels, keys=('private-address',)):
@@ -117,26 +121,29 @@ def render_upstart(name, data):
     return True
 
 
-def register_machine(apiserver):
+def register_machine(apiserver, retry=False):
     parsed = urlparse.urlparse(apiserver)
     headers = {"Content-type": "application/json",
                "Accept": "application/json"}
-    identity = hookenv.local_unit().replace('/', '-'),
+    #identity = hookenv.local_unit().replace('/', '-')
+    private_address = hookenv.unit_private_ip()
 
     with open('/proc/meminfo') as fh:
         info = fh.readline()
         mem = info.strip().split(":")[1].strip().split()[0]
     cpus = os.sysconf("SC_NPROCESSORS_ONLN")
 
-    request = {
+    request = _encode({
         'kind': 'Minion',
-        'id': identity,
-        'hostIP': hookenv.unit_private_ip(),
+        # These can only differ for cloud provider backed instances?
+        'id': private_address,
+        'hostIP': private_address,
         'resources': {
             'capacity': {
                 'mem': mem + ' K',
-                'cpu': cpus}}}
+                'cpu': cpus}}})
 
+    print("Registration request %s" % request)
     conn = httplib.HTTPConnection(parsed.hostname, parsed.port)
     conn.request(
         "POST", "/api/v1beta1/minions",
@@ -144,9 +151,33 @@ def register_machine(apiserver):
         headers)
 
     response = conn.getresponse()
-    print "result", response.read()
-    if not response.status in (200, 202, 409):
+    result = json.loads(response.read())
+    print("Response status:%s reason:%s body:%s" % (
+        response.status, response.reason, result))
+
+    if response.status in (200, 202, 409):
+        print("Registered")
+    elif not retry and response.status in (500,) and result.get(
+            'message', '').startswith('The requested resource does not exist'):
+        # There's something fishy in the kube api here (0.4 dev), first time we
+        # go to register a new minion, we always seem to get this error.
+        # https://github.com/GoogleCloudPlatform/kubernetes/issues/1995
+        time.sleep(1)
+        print("Retrying registration...")
+        return register_machine(apiserver, retry=True)
+    else:
+        print("Registration error")
         raise RuntimeError("Unable to register machine with %s" % request)
+
+
+def setup_kubernetes_group():
+    output = subprocess.check_output(['groups', 'kubernetes'])
+
+    # TODO: check group exists
+    if not 'docker' in output:
+        subprocess.check_output(
+            ['usermod', '-a', '-G', 'docker', 'kubernetes'])
+
 
 if __name__ == '__main__':
     hooks.execute(sys.argv)
